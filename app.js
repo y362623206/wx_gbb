30234d43508b77d7a2fdb68cb5ac93316d452412
y362623206 <47551672+y362623206@users.noreply.github.com>
2019-02-12 17:13:00 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+﻿//app.js 
+var mta = require('mta_analysis.js'); 
+
+App({
+
+  onLaunch: function () {
+    var that=this;
+ mta.App.init({
+      "appID": "500633897",
+      "eventID": "500633900",
+      "statPullDownFresh": true,
+      "statShareApp": true,
+      "statReachBottom": true
+    });
+     // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              var userInfo = res.userInfo;
+              console.log(userInfo);
+              wx.setStorage({
+                key: "userInfo",
+                data: JSON.stringify(userInfo),
+              });
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+
+    
+    if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function () {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              showCancel:false,
+              success: function (res) {
+                  // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                  updateManager.applyUpdate()
+                
+              }
+            })
+          })
+          updateManager.onUpdateFailed(function () {
+            // 新的版本下载失败
+            wx.showModal({
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~',
+            })
+          })
+        }
+      })
+    } else {
+      // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+    
+  },
+  globalData: {
+    userInfo: null,
+  }
+})
